ae56f9c0051f296337da829f2cefa416ec3dc384
Xaver <881729+evrpress@users.noreply.github.com>
2023-09-22 17:53:40 +0200

    - fixed: issue with latest block forms plugin not shoing settings page - tested: up to WordPress 6.3

--- mailster-hcaptcha.php
+++ mailster-hcaptcha.php
@@ -3,13 +3,13 @@
 Plugin Name: Mailster hCaptcha
 Plugin URI: https://mailster.co/?utm_campaign=wporg&utm_source=wordpress.org&utm_medium=plugin&utm_term=hCaptcha
 Description: Adds a hCaptcha to your Mailster Subscription forms
-Version: 2.0.0
+Version: 2.0.1
 Author: EverPress
 Author URI: https://mailster.co
 Text Domain: mailster-hcaptcha
 License: GPLv2 or later
 */
-define( 'MAILSTER_HCAPTCHA_VERSION', '2.0.0' );
+define( 'MAILSTER_HCAPTCHA_VERSION', '2.0.1' );
 define( 'MAILSTER_HCAPTCHA_REQUIRED_VERSION', '3.3.3' );
 define( 'MAILSTER_HCAPTCHA_FILE', __FILE__ );
 
